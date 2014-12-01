58f20a99b548c54724badbb06f608c4063448113
jiangzq1023 <jzq1023@163.com>
2014-12-01 14:52:05 +0800

    导入工程代码

---
+++ src/main/java/org/govern/ccms/aaa.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.govern.ccms;
+
+/**
+ * @author zqjiang
+ *
+ */
+public class aaa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
